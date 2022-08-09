c5a1efd46de6fa8cce07d738d36fc759da73ddd9
colendiosman <canberk.gecgel@colendi.com>
2022-08-09 14:41:58 +0300

    service base methods added

--- src/main/java/com/colendi/insurance/base/services/ApplicationAgreementsService.java
+++ src/main/java/com/colendi/insurance/base/services/ApplicationAgreementsService.java
@@ -1,7 +1,42 @@
 package com.colendi.insurance.base.services;
 
+import com.colendi.insurance.base.exceptions.AccountNotFoundException;
+import com.colendi.insurance.base.models.ApplicationAgreements;
+import com.colendi.insurance.base.repositories.ApplicationAgreementsRepository;
+import com.colendi.insurance.base.repositories.ApplicationProductPermissionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ApplicationAgreementsService {
+
+    @Autowired
+    private ApplicationAgreementsRepository repository;
+
+    public ApplicationAgreementsService(){}
+
+    public ApplicationAgreements getApplicationAgreementById(Long id){
+        return null;
+    }
+
+    public List<ApplicationAgreements> getApplicationAgreements(){
+        return null;
+    }
+
+    public ApplicationAgreements saveApplicationAgreement(ApplicationAgreements applicationAgreements){
+        return null;
+    }
+
+    public ApplicationAgreements updateApplicationAgreement(ApplicationAgreements applicationAgreements){
+        return null;
+    }
+
+    public ApplicationAgreements deleteApplicationAgreementById(){
+        return null;
+    }
+
+
 }
